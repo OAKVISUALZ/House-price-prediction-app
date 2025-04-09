8b657fd98098dd3f0049973a3d8bc70934830be8
Olaleye Aanuoluwapo Kayode <150554406+OAKVISUALZ@users.noreply.github.com>
2025-04-09 06:48:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.express as px
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
